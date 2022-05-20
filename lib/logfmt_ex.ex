78686880dab88706c3e6a14f47464a4378debebb
Andreas Kasprzok <akasprzok@gmail.com>
2022-05-20 14:33:36 -0700

    initial commit

---
+++ lib/logfmt_ex.ex
@@ -0,0 +1,18 @@
+defmodule LogfmtEx do
+  @moduledoc """
+  Documentation for `LogfmtEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LogfmtEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/logfmt_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule LogfmtExTest do
+  use ExUnit.Case
+  doctest LogfmtEx
+
+  test "greets the world" do
+    assert LogfmtEx.hello() == :world
+  end
+end
